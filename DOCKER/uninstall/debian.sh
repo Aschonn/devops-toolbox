ddf662aa606a708e3505a4a3c1334719d3a6ea3e
Aschonn <aschonn_trinity@yahoo.com>
2023-01-14 20:31:26 -0600

    first commit

---
+++ DOCKER/uninstall/debian.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get purge -y docker-ce docker-ce-cli
+sudo apt-get autoremove -y --purge docker-ce docker-ce-cli
+sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli
+sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce
+sudo rm -rf /var/lib/docker /etc/docker
+sudo rm /etc/apparmor.d/docker
+sudo groupdel docker
+sudo rm -rf /var/run/docker.sock
